ea1f238421719f4ec422458742a01b0b622c075b
TienDat-3093 <tiendathuynh245@gmail.com>
2023-11-28 15:43:22 +0700

    add_model_product_types

--- backend/app/Http/Controllers/Web/AdminsController.php
+++ backend/app/Http/Controllers/Web/AdminsController.php
@@ -10,7 +10,7 @@ class AdminsController extends Controller
 {
     public function dashboard()
     {
-        return view('admin/dashboard');
+        return view('dashboard');
     }
     public function login()
     {
