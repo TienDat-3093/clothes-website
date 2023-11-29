1e4387825a7366b14752ce5b70b654a7c4e49611
HuynhTam2804 <tambemi2k3@gmail.com>
2023-11-29 14:48:38 +0700

    add_product_types_views

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\AdminsController;
+use App\Http\Controllers\Web\ProductTypesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,18 +15,42 @@ use App\Http\Controllers\Web\AdminsController;
 */
 
 /* Route::get('/', function () {
-    return view('admin/dashboard');
+    return view('dashboard');
 }); */
 
 Route::middleware('guest')->group(function () {
-    Route::name('admin.')->group(function () {
-        Route::get('/login', [AdminsController::class, 'login'])->name('login');
-        Route::post('/login', [AdminsController::class, 'loginHandle'])->name('loginHandle');
-    });
+
+    Route::get('/', [AdminsController::class, 'login'])->name('login');
+    Route::post('/login', [AdminsController::class, 'loginHandle'])->name('loginHandle');
+    
 });
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [AdminsController::class, 'dashboard'])->name('admin.dashboard');
-    Route::get('/logout', [AdminsController::class, 'logout'])->name('admin.logout');
+
+    Route::get('/dashboard', [AdminsController::class, 'dashboard'])->name('dashboard');
+    Route::get('/logout', [AdminsController::class, 'logout'])->name('logout');
+
+    
+    //ProductTypes
+
+    Route::prefix('/product-types')->group(function(){
+
+        Route::name('product-types.')->group(function(){
+
+            Route::get('index',[ProductTypesController::class,'List'])->name('index');
+            
+            Route::get('create',[ProductTypesController::class,'Create'])->name('create');
+            Route::post('create',[ProductTypesController::class,'createHandler'])->name('create-handler');
+            
+            Route::get('update/{id}',[ProductTypesController::class,'Update'])->name('update');
+            Route::post('update/{id}',[ProductTypesController::class,'updateHandler'])->name('update-handler');
+            
+            Route::get('delete/{id}',[ProductTypesController::class,'Delete'])->name('delete');
+
+        });
+
+    });
+
+
 });
