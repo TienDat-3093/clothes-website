8f992b7e7bbdb212f052e49a542b39c3cc54ef67
HuynhTam2804 <tambemi2k3@gmail.com>
2024-01-10 21:19:10 +0700

    add/cart

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,20 +1,20 @@
-import React, { useState,useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Slider from "../components/home/Slider";
 import Banner from "../components/home/Banner";
 import ListProducts from "../components/ListProducts";
 import Footer from "../components/Footer";
-import {fetchAllProduct} from "../services/UserService";
+import { fetchAllProduct } from "../services/UserService";
+
 export default function Home() {
-  const [listProducts,setProducts] = useState([]);
+  const [listProducts, setProducts] = useState([]);
   useEffect(() => {
     //getUser();
     getProduct();
   }, []);
-  const getProduct  =async()=>{
+  const getProduct = async () => {
     let res = await fetchAllProduct();
-    if(res && res.data && res.data.data)
-    {
+    if (res && res.data && res.data.data) {
       setProducts(res.data.data);
     }
   }
@@ -22,10 +22,10 @@ export default function Home() {
     <>
       <div className="animsition">
         <Header />
-        <Slider/>
-        <Banner/>
-        <ListProducts data={listProducts}/>
-        <Footer/>
+        <Slider />
+        <Banner />
+        <ListProducts data={listProducts} />
+        <Footer />
       </div>
     </>
   );
--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -3,73 +3,73 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export default function Detail() {
-const user = JSON.parse(localStorage.getItem('user'));
-  return (
-    <>
-      <div>
-        <Header />
-      <section className="sec-product-detail bg0 p-t-100 p-b-60">
-        <div className="container">
-        <div>
-            User Infomations
-        </div>
-        <form>
+    const user = JSON.parse(localStorage.getItem('user'));
+    return (
+        <>
             <div>
-              <div className="form-group">
-                <label>Username</label>
-                <input
-                  type="text"
-                  className="form-control border-input"
-                  disabled=""
-                  placeholder="Username"
-                  defaultValue={user.username}
-                />
-              </div>
-              <div className="form-group">
-                <label>Fullname</label>
-                <input
-                  type="text"
-                  className="form-control border-input"
-                  disabled=""
-                  placeholder="Fullname"
-                  defaultValue={user.fullname}
-                />
-              </div>
-              <div className="form-group">
-                <label>Email</label>
-                <input
-                  type="text"
-                  className="form-control border-input"
-                  disabled=""
-                  placeholder="Email"
-                  defaultValue={user.email}
-                />
-              </div>
-              <div className="form-group">
-                <label>Password</label>
-                <input
-                  type="text"
-                  className="form-control border-input"
-                  disabled=""
-                  placeholder="Password"
-                  defaultValue="***"
-                />
-              </div>
-              <div className="form-group">
-                <label>Phone Number</label>
-                <input
-                  type="text"
-                  className="form-control border-input"
-                  disabled=""
-                  placeholder="Phone Number"
-                  defaultValue={user.phone_number}
-                />
-              </div>
+                <Header />
+                <section className="sec-product-detail bg0 p-t-100 p-b-60">
+                    <div className="container">
+                        <div>
+                            User Infomations
+                        </div>
+                        <form>
+                            <div>
+                                <div className="form-group">
+                                    <label>Username</label>
+                                    <input
+                                        type="text"
+                                        className="form-control border-input"
+                                        disabled=""
+                                        placeholder="Username"
+                                        defaultValue={user.username}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Fullname</label>
+                                    <input
+                                        type="text"
+                                        className="form-control border-input"
+                                        disabled=""
+                                        placeholder="Fullname"
+                                        defaultValue={user.fullname}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Email</label>
+                                    <input
+                                        type="text"
+                                        className="form-control border-input"
+                                        disabled=""
+                                        placeholder="Email"
+                                        defaultValue={user.email}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Password</label>
+                                    <input
+                                        type="text"
+                                        className="form-control border-input"
+                                        disabled=""
+                                        placeholder="Password"
+                                        defaultValue="***"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Phone Number</label>
+                                    <input
+                                        type="text"
+                                        className="form-control border-input"
+                                        disabled=""
+                                        placeholder="Phone Number"
+                                        defaultValue={user.phone_number}
+                                    />
+                                </div>
+                            </div>
+                        </form></div>
+                </section>
+                <Footer />
             </div>
-        </form></div>
-        </section>
-        <Footer />
-      </div>
-    </>
-  );
+        </>
+    );
 }
